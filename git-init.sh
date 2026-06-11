556daf65f9de6a5689e4702f27901bc166752adc
David Brooks <dbrooks@macbook-pro.vhf.sh>
2026-06-11 16:21:31 -0400

    Make vault pre-commit hook discover vault files

--- git-init.sh
+++ git-init.sh
@@ -8,28 +8,34 @@
 #
 set -e
 
-vaults=(
-  "group_vars/all/vault.yaml"
-  "host_vars/servo/vault.yaml"
-  "host_vars/tailscale/vault.yaml"
-)
-
 precommit=".git/hooks/pre-commit"
 
-# Build the pre-commit hook script
-hook="#!/usr/bin/env bash\n"
-for vault in "${vaults[@]}"; do
-  hook+="if ( git show :${vault} | grep -q \"\\\$ANSIBLE_VAULT;\" ); then\n"
-  hook+="  echo \"\033[38;5;108m${vault} encrypted. Safe to commit.\033[0m\"\n"
-  hook+="else\n"
-  hook+="  echo \"\033[38;5;208m${vault} not encrypted! Run 'make encrypt' and try again.\033[0m\"\n"
-  hook+="  exit 1\n"
-  hook+="fi\n"
-done
-
 if [ -d .git/ ]; then
-	rm -f $precommit
-	echo -e "$hook" > $precommit
+  cat > "$precommit" <<'HOOK'
+#!/usr/bin/env bash
+#
+# Prevent plaintext Ansible vault files from being committed.
+#
+set -e
+
+vaults=()
+while IFS= read -r path; do
+  case "$path" in
+    group_vars/*/vault.yaml|host_vars/*/vault.yaml)
+      vaults+=("$path")
+      ;;
+  esac
+done < <(git diff --cached --name-only --diff-filter=ACMR)
+
+for vault in "${vaults[@]}"; do
+  if git show ":${vault}" | grep -q '^\$ANSIBLE_VAULT;'; then
+    printf '\033[38;5;108m%s encrypted. Safe to commit.\033[0m\n' "$vault"
+  else
+    printf '\033[38;5;208m%s not encrypted! Run '"'"'make encrypt'"'"' and try again.\033[0m\n' "$vault"
+    exit 1
+  fi
+done
+HOOK
 fi
 
-chmod +x $precommit
+chmod +x "$precommit"
